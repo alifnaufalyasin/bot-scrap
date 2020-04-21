f9b7df1f3776722ed3345a2f7515d988d8f5155d
alifnaufalyasin <alifnaufalyasin@gmail.com>
2020-04-21 12:51:35 +0700

    INIT File

---
+++ handler/message.js
@@ -0,0 +1,23 @@
+async function handleMessage(Context) {
+  const message = Context.event.message;
+  let profileUser = await Context.getUserProfile()
+  let userId = profileUser.userId;
+  // console.log(profileUser);
+  if (message.type !== "text") {
+    Context.reply([{
+      type: "text",
+      text: "Kakak ngirim apa? aku gabisa liat :( "
+    }]);
+  } else {
+    // console.log(userId);
+    const kalimat = message.text.toLowerCase()
+    const kata = kalimat.match(/http/g)
+    if(kata){
+      Context.sendText('Ada')
+    }
+  }
+}
+
+module.exports = {
+  handleMessage
+};
---
+++ index.js
@@ -0,0 +1,10 @@
+const { postback } = require("./handler/postback")
+const { handleMessage } = require("./handler/message")
+
+module.exports = async function App(context) {
+  if (context.event.isPosback){
+    return postback(context);
+  }else{
+    return handleMessage(context);
+  }
+}
