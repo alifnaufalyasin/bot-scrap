f2cc2e11fcdd71e449f5b587f5ebc1c4750cb4d4
alifnaufalyasin <alifnaufalyasin@gmail.com>
2020-04-21 21:32:11 +0700

    Update

--- controller/link.js
+++ controller/link.js
@@ -3,12 +3,16 @@ const { listLink } = require("../model/modelListLink")
 async function add(context) {
   const id = await listLink.countDocuments({})
   let profileUser = await context.getUserProfile()
+  const date = new Date(context.event.date)
+  const dd = String(date.getDate()).padStart(2, "0")
+  const mm = String(date.getMonth()).padStart(2, "0")
+  const yyyy = date.getFullYear()
   const myobj = { 
     id_link: id+1, 
     text: context.event.message.text, 
     pengirim: profileUser.displayName,
     user_id: profileUser.userId,
-    waktu: context.event.date, 
+    waktu: yyyy+'-'+dd+'-'+mm, 
   };
   const data = new listLink(myobj)
   data.save()
